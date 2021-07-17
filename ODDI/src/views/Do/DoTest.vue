e1b301ab48369aa0211517dbab408319fa4ea972
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-07-17 10:34:07 +0900

    테이블 기능 추가
    - 페이지네이션
    - 행 셀렉트

--- ODDI/src/views/Do/DoTest.vue
+++ ODDI/src/views/Do/DoTest.vue
@@ -1,34 +1,115 @@
 <template>
-  <b-card>
-    <!-- b-card header은 필요에 따라 바꾸도록 하자 -->
-    <b-card-header class="bg-transparent border-0">
-      <h3 class="mb-0">Dark table</h3>
-    </b-card-header>
+  <div>
+    <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
+      <!-- Card stats -->
+      <b-row>
+        <b-col>
+          <stats-card
+            title="Total traffic"
+            type="gradient-red"
+            sub-title="350,897"
+            icon="ni ni-active-40"
+            class="mb-4"
+          >
+            <template slot="footer">
+              <span class="text-success mr-2">3.48%</span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </b-col>
+        <b-col xl="3" md="6">
+          <stats-card
+            title="Total traffic"
+            type="gradient-orange"
+            sub-title="2,356"
+            icon="ni ni-chart-pie-35"
+            class="mb-4"
+          >
+            <template slot="footer">
+              <span class="text-success mr-2">12.18%</span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </b-col>
+        <b-col xl="3" md="6">
+          <stats-card
+            title="Sales"
+            type="gradient-green"
+            sub-title="924"
+            icon="ni ni-money-coins"
+            class="mb-4"
+          >
+            <template slot="footer">
+              <span class="text-danger mr-2">5.72%</span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </b-col>
+        <b-col xl="3" md="6">
+          <stats-card
+            title="Performance"
+            type="gradient-info"
+            sub-title="49,65%"
+            icon="ni ni-chart-bar-32"
+            class="mb-4"
+          >
+            <template slot="footer">
+              <span class="text-success mr-2">54.8%</span>
+              <span class="text-nowrap">Since last month</span>
+            </template>
+          </stats-card>
+        </b-col>
+      </b-row>
+    </base-header>
 
-    <table class="table table-light-hover" header-row-class-name="thead-light">
-      <thead>
-        <tr>
-          <th>선택</th>
-          <th>이벤트명</th>
-          <th>이벤트 날짜</th>
-          <th>참여방법</th>
-          <th>참여자 수</th>
-          <th>조회수</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :key="i" v-for="(event, i) in eventList">
-          <td><input type="checkbox" name="" id="" /></td>
-          <td>{{ event.EventName }}</td>
-          <td>{{ event.EventStartTime }}</td>
-          <td>{{ event.EventType }}</td>
-          <td>{{ event.EventPersonNum }}</td>
-          <td>{{ event.EventPersonNum }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <b-card-footer> </b-card-footer>
-  </b-card>
+    <!-- 이벤트 리스트 테이블 입니다 -->
+    <b-card class="border-0">
+      <b-card-header class="border-0">
+        <h3 class="mb-0">Dark table</h3>
+      </b-card-header>
+
+      <b-table
+        hover
+        sticky-header
+        :items="eventList"
+        :fields="fields"
+        id="my-table"
+        :current-page="currentPage"
+        :per-page="perPage"
+        :select-mode="selectMode"
+        selectable
+        @row-selected="onRowSelected"
+      >
+        <!-- 테이블 행 셀렉트 기능입니다. -->
+        <template #cell(selected)="{ rowSelected }">
+          <template v-if="rowSelected">
+            <span aria-hidden="true">&check;</span>
+            <span class="sr-only">Selected</span>
+          </template>
+          <template v-else>
+            <span aria-hidden="true">&nbsp;</span>
+            <span class="sr-only">Not selected</span>
+          </template>
+        </template>
+      </b-table>
+      <!-- 셀렉트 된 행을 보여주는 기능입니다. -->
+      <p>
+        Selected Rows:<br />
+        {{ selected }}
+      </p>
+
+      <b-card-footer>
+        <!-- 페이지네이션 기능입니다. -->
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          aria-controls="my-table"
+        >
+        </b-pagination>
+      </b-card-footer>
+    </b-card>
+  </div>
 </template>
 
 <script>
@@ -37,21 +118,66 @@ export default {
   components: {},
   data() {
     return {
-      eventList: []
+      //이벤트 리스트 필드 입니다
+      fields: [
+        {
+          key: "Selected",
+          label: "선택"
+        },
+        {
+          key: "EventName",
+          label: "이벤트명"
+        },
+        {
+          key: "EventStartTime",
+          label: "이벤트시간"
+        },
+        {
+          key: "EventType",
+          label: "이벤트유형"
+        },
+        {
+          key: "EventPersonNum",
+          label: "참여자 수"
+        },
+        {
+          key: "EventPrise",
+          label: "조회수"
+        }
+      ],
+      //페이지네이션 기능입니다.
+      perPage: 10,
+      currentPage: 1,
+      eventList: [],
+      //테이블 행 셀렉트 기능입니다.
+      selectMode: "single",
+      selected: []
     };
   },
   created() {
+    //이벤트 리스트 테이블 기능입니다.
     this.getList();
+  },
+  computed: {
+    //페이지 네이션 기능입니다.
+    rows() {
+      return this.eventList.length;
+    }
   },
   mounted() {},
   updated() {},
   unmounted() {},
   methods: {
+    //이벤트 리스트 Mock 랜더링 기능입니다.
     async getList() {
       this.eventList = await this.$api(
         "https://4d20c04e-97aa-4c1c-b612-4fe31a4b811c.mock.pstmn.io/eventlist",
         "get"
       );
+    },
+    // 테이블 행 셀렉트 기능입니다.
+    onRowSelected(items) {
+      this.selected = items;
     }
   }
 };
