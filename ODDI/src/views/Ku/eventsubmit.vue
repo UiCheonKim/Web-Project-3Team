a68e317b7a5820e2a33294324a16cf5b81f69d77
JayoungKooo <86044261+JayoungKooo@users.noreply.github.com>
2021-07-19 10:15:55 +0900

    .

--- ODDI/src/views/Ku/eventsubmit.vue
+++ ODDI/src/views/Ku/eventsubmit.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
-    </base-header>
+    <base-header class="pb-6 pt-md-5 bg-gradient-success"> </base-header>
     <br />
 
     <br />
