7e092622ddd583a53fe5204a3b60ef7ea47dc360
JayoungKooo <86044261+JayoungKooo@users.noreply.github.com>
2021-07-20 12:53:18 +0900

    .

--- ODDI/src/views/Ku/KuTest.vue
+++ ODDI/src/views/Ku/KuTest.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>4조 화이팅</h1>
+  <div style="width:100px">
+    <div class="d-inline">
+      <img
+        class="rounded"
+        height="100%"
+        width="100%"
+        src="https://cools.kr/wp-content/uploads/2021/05/KFC-%ED%95%A0%EC%9D%B8%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EC%9D%B4-%EB%82%A0%EB%A7%8C%EC%9D%84-%EA%B8%B0%EB%8B%A4%EB%A0%A4%EC%98%A8-%EC%82%AC%EB%9E%8C-%EC%86%90-%EA%B8%B0%EB%8B%A4%EB%A6%AC%EA%B3%A0-%EA%B8%B0%EB%8B%A4%EB%A0%B8%EB%8D%98-%EA%B7%B8.jpg"
+        alt="kfc"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -8,10 +16,9 @@ export default {
   name: "",
   components: {},
   data() {
-    return {
-      sampleData: ""
-    };
+    return {};
   },
+
   setup() {},
   created() {},
   mounted() {},
