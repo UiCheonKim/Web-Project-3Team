fb5b7fe2d8d77641c379da9b95cb37ddcedcf9cf
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-07-21 11:45:49 +0900

    insert

---
+++ oddi_back/sql.js
@@ -0,0 +1,28 @@
+module.exports = {
+  list: {
+    query: "select * from t_person",
+  },
+  createUser: {
+    query: "insert into t_person set ?", // insert into t_person set first_name='Kim', last_name='Uicheon', email='drooom@naver.com'
+    // 물음표 2개는 값 2개를 준다
+  },
+  deletePerson: {
+    query: "delete from t_person where id=?",
+  },
+  updatePerson: {
+    query: "update t_person set ? where id=?",
+  },
+  //_________________________________________________________________________________
+  create_EventTable: {
+    query: "insert into t_event set ?",
+  },
+  create_PhotoTable: {
+    query: "insert into t_photo set ?",
+  },
+  create_BuisnessTable: {
+    query: "insert into t_buisness set ?",
+  },
+  create_UserTable: {
+    query: "insert into t_user set ?",
+  },
+};
