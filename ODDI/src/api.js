e683239898a5b2732f9309191c4caf538d6ed4e0
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-07-21 15:56:48 +0900

    db

---
+++ ODDI/src/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+export default {
+  methods: {
+    async $api(url, method, data) {
+      return (
+        await axios({
+          method: method,
+          url: url,
+          data: data
+        }).catch(e => {
+          console.log(e);
+        })
+      ).data;
+    },
+    async $upload(url, file) {
+      const formData = new FormData();
+      formData.append("attachment", file);
+      //formData.append("name", Uicheonkim);
+      return (
+        await axios
+          .post(url, formData, {
+            // 밑에 안써주면 안넘어감 multipart/form-data 이걸 넣어줘야 넘어간다
+            headers: { "Content-Type": "multipart/form-data" }
+          })
+          .catch(e => {
+            console.log(e);
+          })
+      ).data;
+    }
+  }
+};
